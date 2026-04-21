c6eb9606275b17fe8e908d39b7357d99d90e747b
we5046 <we5046@naver.com>
2026-04-21 10:23:31 +0900

    learn a.m. part1

---
+++ study/src/designPattern/template/Main.java
@@ -0,0 +1,11 @@
+package designPattern.template;
+
+public class Main {
+    public static void main(String[] args) {
+        AbstractDisplay d1 = new CharDisplay('H');
+        AbstractDisplay d2 = new StringDisplay("KB IT's Your Life");
+
+        d1.display();
+        d2.display();
+    }
+}
